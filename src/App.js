b84fc077afadcb861227458238c12c67d47917e1
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-22 14:59:22 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './Pages/Home/Home/Home';
+
+function App() {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './Home.css';
+
+const Home = () => {
+    const [selectColors, setSelectColors] = useState([]);
+
+    const handleAddColor = event => {
+        const colorName = event.target.innerText;
+        const colors = [...selectColors, colorName];
+        setSelectColors(colors);
+
+        const element = event.target.parentNode;
+        element.removeChild(element.children[0]);
+    }
+
+    return (
+        <div className='container'>
+            <div className='selected-color'>
+                {
+                    selectColors.map(color =>
+                        <span
+                            className={`set-margin ${color.toLowerCase()} bg-color`}>{color} x</span>)
+                }
+            </div>
+            <div className='color-list'>
+                <table>
+                    <tbody>
+                        <tr onClick={handleAddColor} className='purple'>
+                            <td>
+                                <span>Purple</span>
+                            </td>
+                        </tr>
+                        <tr onClick={handleAddColor} className='red'>
+                            <td>
+                                <span>Red</span>
+                            </td>
+                        </tr>
+                        <tr onClick={handleAddColor} className='orange'>
+                            <td>
+                                <span>Orange</span>
+                            </td>
+                        </tr>
+                        <tr onClick={handleAddColor} className='green'>
+                            <td>
+                                <span>Green</span>
+                            </td>
+                        </tr>
+                        <tr onClick={handleAddColor} className='slate'>
+                            <td>
+                                <span>Slate</span>
+                            </td>
+                        </tr>
+                        <tr onClick={handleAddColor} className='oliver'>
+                            <td>
+                                <span>Oliver</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
